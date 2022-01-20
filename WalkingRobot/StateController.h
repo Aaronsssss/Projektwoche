3e48aa1c68a1d7127316a7b7f857a42858a71c3d
Aaronsssss <94520407+Aaronsssss@users.noreply.github.com>
2022-01-20 16:16:48 +0100

    State Controller added

--- WalkingRobot/StateController.h
+++ WalkingRobot/StateController.h
@@ -2,46 +2,71 @@
 #define STATECONTROLLER_H__
 
 #include "Controller.h"
+#include <stdlib.h>
 
-class StateController() {
+class StateController {
   private:
-  uint32_t m_Distance;
-  Controller m_Controller;
-  uint32_t m_State;
-  uint32_t m_EscapeDistanceWalk = 40;
-  uint32_t m_EscapeDistanceTurn = 60;
-  /*
-   * 1 == MoveForward
-   * 2 == Drehen (vorbereitung)
-   * 3 == LinksDrehen
-   * 4 == RechtsDrehen
-   */
-  
-  public:
-  enum {
-    FORWARD     = 1,
-    TURN        = 2,
-    RIGHTTURN   = 3,
-    LEFTTURN
-  }
-  
-  StateController() {
-    m_State = 1;
-  }
-  ~StateController() {
-  }
+    uint32_t m_Distance;
+    //Controller m_Controller;
+    uint32_t m_State;
+    uint32_t m_EscapeDistanceWalk = 40;
+    uint32_t m_EscapeDistanceTurn = 60;
 
-  void checkState() {
+  public:
+    enum {
+      FORWARD     = 1,
+      TURN        = 2,
+      RIGHTTURN   = 3,
+      LEFTTURN    = 4
+    };
+
+    StateController() {
+      m_State = 1;
+    }
+    ~StateController() {
+    }
+
+    void checkState() {
+      uint32_t distance = m_Controller.MeasureDistance();
+
+      if (m_State = FORWARD && m_Distance < m_EscapeDistanceWalk) {
+        m_State = TURN;
+      } else if ((m_State == RIGHTTURN || m_State == LEFTTURN) && m_Distance == m_EscapeDistanceTurn) {
+        m_State = FORWARD;
+      }
+
+      if (m_State == TURN) {
+        if ((rand() % 2) != 0) {
+          m_State = RIGHTTURN;
+          Serial.println("Switch zu Right");
+        } else {
+          m_State = LEFTTURN;
+          Serial.println("Switch zu Left");
+        }
+      }
+    }
     
-    uint32_t distance = m_Controller.MeasureDistance();
-    if (m_Distance < 60 && m_Distance >40)
-    if (m_State = 1 && m_Distance > 40) {
-      
-    } else if (distance < 40 && m_State = 1) {
-      m_State = 2;
-    } else if (distance > 60
-    
-  }
-  
+    void walk() {
+      switch (m_State) {
+        case FORWARD:
+         // m_Controller.stepForward();
+          Serial.println("Step Forward");
+          break;
+
+        case RIGHTTURN:
+         // m_Controller.moveRight();
+          Serial.println("Move Right");
+          break;
+
+        case LEFTTURN:
+         // m_Controller.moveLeft();
+          Serial.println("Move Left");
+          break;
+
+        default:
+          break;
+      }
+    }
+
 };
 #endif
